8f561dccf0f6932026c0bf32e26a9b096302559b
kalerie <valeriiakuryliak@gmail.com>
2024-11-27 22:16:49 +0100

    - added sidebar & mobile nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-
-      <UserButton afterSignOutUrl='/' />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
